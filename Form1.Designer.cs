407532e6e08dd511fc71dafc055823bb867331c1
astrandr <andreyb2001@yahoo.com>
2018-02-13 14:31:20 +0200

    CoolTable

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.xDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.xDataGridViewTextBoxColumn,
+            this.yDataGridViewTextBoxColumn});
+            this.dataGridView1.DataMember = "Table1";
+            this.dataGridView1.DataSource = this.dataSet1;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(310, 203);
+            this.dataGridView1.StandardTab = true;
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            this.dataGridView1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.dataGridView1_PreviewKeyDown);
+            // 
+            // xDataGridViewTextBoxColumn
+            // 
+            this.xDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.xDataGridViewTextBoxColumn.DataPropertyName = "X";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.xDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle6;
+            this.xDataGridViewTextBoxColumn.HeaderText = "X";
+            this.xDataGridViewTextBoxColumn.MinimumWidth = 45;
+            this.xDataGridViewTextBoxColumn.Name = "xDataGridViewTextBoxColumn";
+            this.xDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.xDataGridViewTextBoxColumn.Width = 45;
+            // 
+            // yDataGridViewTextBoxColumn
+            // 
+            this.yDataGridViewTextBoxColumn.DataPropertyName = "Y";
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.yDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle7;
+            this.yDataGridViewTextBoxColumn.HeaderText = "Y";
+            this.yDataGridViewTextBoxColumn.Name = "yDataGridViewTextBoxColumn";
+            this.yDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "X";
+            this.dataColumn1.DataType = typeof(double);
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "Y";
+            this.dataColumn2.DataType = typeof(double);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 615);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
+            this.button1.Enter += new System.EventHandler(this.button1_Enter);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 615);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView2.Location = new System.Drawing.Point(41, 268);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 247);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 658);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.DataSet dataSet1;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridView
+{
+    public partial class Form1 : Form
+    {
+
+        Rectangle buttonRectangle = Rectangle.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.SuspendLayout();
+            int tickStart = Environment.TickCount;
+            for (int i = 0; i < 400; i++)
+            {
+                DataRow newRow = dataTable1.NewRow();
+                newRow["X"] = 1;
+                newRow["Y"] = 1;
+                dataTable1.Rows.Add(newRow);
+            }
+            int tickEnd = Environment.TickCount;
+            int tickSpam = tickEnd - tickStart;
+
+            dataGridView1.ResumeLayout();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.SuspendLayout();
+            dataGridView1.DataSource = null;
+            int tickStart = Environment.TickCount;
+            for (int i = 0; i < 400; i++)
+            {
+                dataGridView1.Rows.Add(new float[] { 1, 1 });
+            }
+            int tickEnd = Environment.TickCount;
+            int tickSpam = tickEnd - tickStart;
+
+            dataGridView1.ResumeLayout();
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.ColumnIndex == -1)
+            {
+                e.Graphics.FillRectangle(
+                        new LinearGradientBrush(
+                            new Point(e.CellBounds.Left, e.CellBounds.Top),
+                            new Point(e.CellBounds.Left, e.CellBounds.Bottom),
+                            Color.FromArgb(255, 160, 160, 160),
+                            Color.FromArgb(255, 190, 190, 190)
+                            ),
+                            e.CellBounds);
+                if (e.RowIndex >= 0)
+                {
+                    e.Graphics.DrawString((e.RowIndex + 1).ToString(), new Font("Arial", 10), new SolidBrush(Color.Black),
+                                            e.CellBounds.Right - e.Graphics.MeasureString((e.RowIndex + 1).ToString(), new Font("Arial", 10)).Width-4,
+                                            (e.CellBounds.Top + e.CellBounds.Bottom) / 2 - e.Graphics.MeasureString(e.ColumnIndex.ToString(), new Font("Arial", 10)).Height / 2);
+                }
+                else
+                {
+                    e.Graphics.DrawString("Row", new Font("Arial", 10), new SolidBrush(Color.Black),
+                                            e.CellBounds.Right - e.Graphics.MeasureString("Row", new Font("Arial", 10)).Width-4,
+                                            (e.CellBounds.Top + e.CellBounds.Bottom) / 2 - e.Graphics.MeasureString(e.ColumnIndex.ToString(), new Font("Arial", 10)).Height / 2);
+                }
+            }
+            else
+                if (e.RowIndex == -1)
+                {
+                    e.Graphics.FillRectangle(
+                            new LinearGradientBrush(
+                                new Point(e.CellBounds.Left, e.CellBounds.Top),
+                                new Point(e.CellBounds.Left, e.CellBounds.Bottom),
+                                Color.FromArgb(255, 180, 180, 180),
+                                Color.FromArgb(255, 220, 220, 220)
+                                ),
+                                e.CellBounds);
+                }
+            else
+                if (e.State.HasFlag(DataGridViewElementStates.Selected))
+                {
+                    e.Graphics.FillRectangle(
+                        new LinearGradientBrush(
+                            new Point(e.CellBounds.Left, e.CellBounds.Top),
+                            new Point(e.CellBounds.Left, e.CellBounds.Bottom),
+                            Color.FromArgb(255, 200, 200, 250),
+                            Color.FromArgb(255, 190, 190, 250)
+                            ),
+                            e.CellBounds);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(
+                        new LinearGradientBrush(
+                            new Point(e.CellBounds.Left, e.CellBounds.Top),
+                            new Point(e.CellBounds.Left, e.CellBounds.Bottom),
+                            Color.FromArgb(255, 240, 240, 240),
+                            Color.FromArgb(255, 250, 250, 250)),
+                            e.CellBounds);
+                }
+            Rectangle cellRect = e.CellBounds;  cellRect.Offset(-1, -1);
+
+            e.Graphics.DrawRectangle(new Pen(Color.DarkGray, 2), cellRect);
+            cellRect.Inflate(-1, -1);
+            e.Graphics.DrawRectangle(new Pen(Color.Black, 1), cellRect);
+            e.PaintContent(e.CellBounds);
+            e.Handled = true;
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+
+            buttonRectangle = e.ClipRectangle;
+
+            e.Graphics.FillRectangle(new LinearGradientBrush(
+                                                            new Point(e.ClipRectangle.Left, e.ClipRectangle.Top),
+                                                            new Point(e.ClipRectangle.Left, e.ClipRectangle.Bottom),
+                                                            Color.FromArgb(255, 180, 180, 180),
+                                                            Color.FromArgb(255, 220, 220, 220)), e.ClipRectangle);
+            e.Graphics.DrawRectangle(new Pen(Color.Gray, 2), e.ClipRectangle);
+            buttonRectangle.Offset(-1, -1);
+            e.Graphics.DrawRectangle(new Pen(Color.Gray, 1), buttonRectangle);
+            e.Graphics.DrawString("Calculate",
+                                    new Font("Arial", 11, FontStyle.Bold | FontStyle.Italic),
+                                    new SolidBrush(Color.FromArgb(255, 80, 80, 80)),
+                                    new Point(e.ClipRectangle.Left + 8, e.ClipRectangle.Top + 3));
+            buttonRectangle.Offset(1, 1);
+        }
+
+        private void button1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            Graphics buttonGraphics = this.button1.CreateGraphics();
+            buttonGraphics.FillRectangle(new LinearGradientBrush(
+                                                            new Point(buttonRectangle.Left, buttonRectangle.Top),
+                                                            new Point(buttonRectangle.Left, buttonRectangle.Bottom),
+                                                            Color.FromArgb(255, 200, 200, 200),
+                                                            Color.FromArgb(255, 250, 250, 250)), buttonRectangle);
+            buttonGraphics.DrawLine(new Pen(Color.Gray, 2), buttonRectangle.Left + 2, buttonRectangle.Bottom - 1, buttonRectangle.Right, buttonRectangle.Bottom - 1);
+            buttonGraphics.DrawLine(new Pen(Color.Gray, 2), buttonRectangle.Right - 1, buttonRectangle.Bottom, buttonRectangle.Right - 1, buttonRectangle.Top + 2);
+            buttonGraphics.DrawString("Calculate",
+                                    new Font("Arial", 11, FontStyle.Bold | FontStyle.Italic),
+                                    new SolidBrush(Color.FromArgb(255, 20, 20, 20)),
+                                    new Point(buttonRectangle.Left + 8, buttonRectangle.Top + 3));
+        }
+
+
+        private void dataGridView1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyValue == 46 && dataGridView1.CurrentCell != null && dataGridView1.CurrentCell.RowIndex < dataGridView1.Rows.Count-1)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.CurrentCell.RowIndex);
+            }
+        }
+    }
+}
