7ae57d3753b89f0c37ffc97eb524c104b4319bc1
astrandr <andreyb2001@yahoo.com>
2018-02-15 16:36:33 +0200

    BackgoundWorker

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace BackgroundWorkerUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(55, 31);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(149, 30);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(55, 78);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(169, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 132);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BackgroundWorkerUI
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker worker = new BackgroundWorker();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            worker.DoWork += new DoWorkEventHandler(Process);
+            worker.ProgressChanged += new ProgressChangedEventHandler(OnStatusChange);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(OnWorkCompleted);
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.RunWorkerAsync(null);
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            this.progressBar.Value = 0;
+        }
+
+        private void Process(object sender, DoWorkEventArgs args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(200);
+                worker.ReportProgress(i);
+                if (worker.CancellationPending) return;
+            }
+        }
+
+        private void OnStatusChange(object sender, ProgressChangedEventArgs args)
+        {
+            this.progressBar.Value = args.ProgressPercentage;
+        }
+
+        private void OnWorkCompleted(object sender, RunWorkerCompletedEventArgs args)
+        {
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+            this.progressBar.Value = 0;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            worker.CancelAsync();
+        }
+    }
+}
